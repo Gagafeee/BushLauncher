2d20cb5c75562610f0acec134d9c9d4445f77f15
Gagafeee <64372089+Gagafeee@users.noreply.github.com>
2022-11-30 08:55:48 +0100

    Add yarn & electron builder

--- src/main.js
+++ src/main.js
@@ -1,16 +1,58 @@
-const { app, BrowserWindow } = require('electron');
-const createWindow = () => {
-    const win = new BrowserWindow({
-        width: 800,
-        height: 600
-    })
+'use strict'
 
-    win.loadFile('src/index.html')
-    win.webContents.openDevTools()
-}
-app.whenReady().then(() => {
-    createWindow()
-})
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit()
+import { app, BrowserWindow } from 'electron'
+import * as path from 'path'
+import { format as formatUrl } from 'url'
+
+const isDevelopment = process.env.NODE_ENV !== 'production'
+
+// global reference to mainWindow (necessary to prevent window from being garbage collected)
+let mainWindow
+
+function createMainWindow() {
+  const window = new BrowserWindow({webPreferences: {nodeIntegration: true}})
+
+  if (isDevelopment) {
+    window.webContents.openDevTools()
+  }
+  else {
+    window.loadURL(formatUrl({
+      pathname: path.join(__dirname, 'index.html'),
+      protocol: 'file',
+      slashes: true
+    }))
+  }
+
+  window.on('closed', () => {
+    mainWindow = null
   })
+
+  window.webContents.on('devtools-opened', () => {
+    window.focus()
+    setImmediate(() => {
+      window.focus()
+    })
+  })
+
+  return window
+}
+
+// quit application when all windows are closed
+app.on('window-all-closed', () => {
+  // on macOS it is common for applications to stay open until the user explicitly quits
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  // on macOS it is common to re-create a window even after all windows have been closed
+  if (mainWindow === null) {
+    mainWindow = createMainWindow()
+  }
+})
+
+// create main BrowserWindow when electron is ready
+app.on('ready', () => {
+  mainWindow = createMainWindow()
+})
